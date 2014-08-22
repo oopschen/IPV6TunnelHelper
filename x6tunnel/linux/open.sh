c524cfc14d326157fb02a26bd1c61c95fa261a42
Ray <linxray@gmail.com>
2014-08-22 17:29:25 +0800

    update linux shell scripts

--- x6tunnel/linux/open.sh
+++ x6tunnel/linux/open.sh
@@ -3,7 +3,7 @@
 scriptdir=$(dirname $0)
 source ${scriptdir}/sudoauto.sh
 
-${cmd_ip} tunnel add name ${1} mode sit remote ${4} local ${2} ttl 128 && \
-${cmd_ip} link set dev ${1} up mtu 1280 && \
+${cmd_ip} tunnel add ${1} mode sit remote ${4} local ${2} ttl 255 && \
+${cmd_ip} link set dev ${1} up && \
 ${cmd_ip} addr add ${3}/64 dev ${1} && \
-${cmd_ip} route add ${6} via ${5} dev ${1}
+${cmd_ip} route add ${6} dev ${1}
