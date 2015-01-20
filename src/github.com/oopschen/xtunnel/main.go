d73c333306603298be40451c20853232c7273863
Ray <linxray@gmail.com>
2015-01-20 21:06:03 +0800

    main entry

--- src/github.com/oopschen/xtunnel/main.go
+++ src/github.com/oopschen/xtunnel/main.go
@@ -1,10 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+/*
+*	<p>xtunnel main entry</p>
+* <p>Usage:</p>
+*	<p>xtunnel open username password</p>
+*	<p>xtunnel close</p>
+ */
 
 func main() {
-	fmt.Println("hello")
-	fmt.Println("1hello")
+	/*
+		TODO
+		parse args
+		get os operator
+
+		if open:
+			init broker
+			get meta info
+			open it
+		else:
+			close it
+	*/
 }
